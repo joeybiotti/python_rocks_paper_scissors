1e09157ec390221c9bb0e3b61bf816ade355d226
Joey Biotti <joey@MacBook-Pro-2.local>
2017-10-24 19:18:43 -0500

    program runs

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import sys
+
 print("Rocks. Paper. Scissors. SHOOT!")
 print("******************************")
 
 user_one = input("Player One, enter your name: ")
-use_two = input("Player Two, enter your name: ")
-user_one_rps = input("%s. Choose rock, paper or scissors" % user_one)
-user_two_rps = input("%s. Choose rock, paper or scissors" % user_two)
+user_two = input("Player Two, enter your name: ")
+user_one_rps = input("%s. Choose rock, paper or scissors: " % user_one)
+user_two_rps = input("%s. Choose rock, paper or scissors: " % user_two)
 
 def compare(user1, user2):
     if user1 == user2:
@@ -19,3 +21,13 @@ def compare(user1, user2):
             return("Scissors win.")
         else:
             return("Rock wins.")
+    elif user1 == 'paper':
+        if user2 == 'rock':
+            return("Paper wins.")
+        else: 
+            return("Scissors win!")
+    else:
+        return("Wrong game. Enter either rock, paper or scissors.")
+        sys.exit()
+
+print(compare(user_one_rps, user_two_rps))
